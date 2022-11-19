3c1b02900c737a5129ef4d9428f6a7bb189881e3
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2022-11-19 23:54:39 +0600

    responsive

--- src/global/components/navbar/Navbar.js
+++ src/global/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styles from "./Navbar.module.css"
 import { Link, useLocation } from "react-router-dom";
 import AnchorLink from "react-anchor-link-smooth-scroll";
@@ -6,23 +6,37 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Navbar = () => {
 
+  const [isActive, setActive] = useState(true);
+
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+
+
 
   return (
 
     <div className={styles.nav_wrap}>
+
       <nav className={styles.main_nav}>
-        <Link to="/">Home</Link>
-        <AnchorLink href="#about">About</AnchorLink>
-        <AnchorLink href="#projects">Projects</AnchorLink>
+        <div className={`${isActive ? styles.hide : styles.show} ${styles.menu}  ${styles.pointer}`}
+          onClick={toggleClass}
+        >Menu</div>
+        <Link className={`${isActive ? styles.show : styles.hide}`} to="/">Home</Link>
+        <AnchorLink className={`${isActive ? styles.show : styles.hide}`} href="#about">About</AnchorLink>
+        <AnchorLink className={`${isActive ? styles.show : styles.hide}`} href="#projects">Projects</AnchorLink>
         {/* <AnchorLink href="#gallery">Gallery</AnchorLink> */}
-        <Link to="/Cv">Resume</Link>
+        <Link className={`${isActive ? styles.show : styles.hide}`} to="/Cv">Resume</Link>
         {/* {user === "Student" && < >
-          <Link to="/issue" state={{ forWhat: "Issue" }}>Issue</Link>
-          <Link to="/issue" state={{ forWhat: "Payment" }}>Payment</Link>
-        </>}
-        {user !== 'Student' && user !== "" && user != null && <Link to="/issue_list">Current Issues</Link>
-        } */}
+            <Link to="/issue" state={{ forWhat: "Issue" }}>Issue</Link>
+            <Link to="/issue" state={{ forWhat: "Payment" }}>Payment</Link>
+          </>}
+          {user !== 'Student' && user !== "" && user != null && <Link to="/issue_list">Current Issues</Link>
+          } */}
+        <div className={`${isActive ? styles.show : styles.hide}  ${styles.pointer}`} onClick={toggleClass} >X</div>
+
       </nav>
+
     </div>
 
   );
--- src/pages/cv/Cv.jsx
+++ src/pages/cv/Cv.jsx
@@ -4,8 +4,13 @@ import style from './Cv.module.css'
 export default function Cv() {
     return (
         <div className={`${style.body}`} >
-            <embed src="assets/pdf/Sourav_Ahmed_f.pdf" width="70%" height="110%"
-            />
+            {/* <embed src="assets/pdf/Sourav_Ahmed_f.pdf" width="70%" height="110%"
+
+            /> */}
+
+            <object data="assets/pdf/Sourav_Ahmed_f.pdf" type="application/pdf" width="70%" height="100%">
+                <p>Alternative text - include a link <a href="https://github.com/Sourav9063/CV/blob/main/Sourav_Ahmed_f.pdf">to the PDF!</a></p>
+            </object>
         </div>
     )
 }
