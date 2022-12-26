221e069a874b14ad6715dd00372673223a54317a
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2022-12-26 17:11:03 +0600

    rive added

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import style from './Contacts.module.css'
+import { useRive } from '@rive-app/react-canvas';
+import keyboard from "./keyboard.riv"
 
 export default function Contacts() {
+
+    const { RiveComponent, rive } = useRive({
+        src: keyboard,
+        autoplay: false,
+    })
+
+
     return (
         <div className={`${style.body}`} >
+            <RiveComponent className={`${style.rive}`}
 
+                onMouseEnter={() => rive && rive.play()}
+            />
         </div>
     )
 }
