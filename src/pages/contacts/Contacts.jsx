bee7fcb0093116d1132baa83dd34fd7d870e34eb
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2023-07-15 02:54:17 +0600

    email form

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,51 +1,65 @@
-import React from 'react'
-import style from './Contacts.module.css'
-import { useRive } from '@rive-app/react-canvas';
-import keyboard from "./keyboard.riv"
-import "../../global/css/global.css"
-import { Link } from 'react-router-dom';
-import GlassDiv from '../../global/components/glassDiv/GlassDiv';
+import React from "react";
+import style from "./Contacts.module.css";
+import { useRive } from "@rive-app/react-canvas";
+import keyboard from "./keyboard.riv";
+import "../../global/css/global.css";
+import { Link } from "react-router-dom";
+import GlassDiv from "../../global/components/glassDiv/GlassDiv";
 import svg from "./ssscribble.svg";
 import svg1 from "./rrrainbow.svg";
 import svg2 from "./ttten.svg";
 import svg3 from "./ssscribble1.svg";
 import svg4 from "./dddepth248.jpg";
 
-
 let pattern = Math.floor(Math.random() * 5) + 1;
 // let pattern = 3
 
 export default function Contacts() {
+  const { RiveComponent, rive } = useRive({
+    src: keyboard,
+    autoplay: false,
+  });
 
-    const { RiveComponent, rive } = useRive({
-        src: keyboard,
-        autoplay: false,
-    })
+  const [isCopied, setIsCopied] = React.useState(false);
 
-    const [isCopied, setIsCopied] = React.useState(false);
+  return (
+    <div
+      className={`${style.body}`}
+      style={{
+        backgroundImage: `url(${
+          pattern === 1
+            ? svg
+            : pattern === 2
+            ? svg1
+            : pattern === 3
+            ? svg2
+            : pattern === 4
+            ? svg3
+            : svg4
+        })`,
+        // backgroundImage: `url(${svg2})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundColor: "#000000",
+      }}
+      onMouseEnter={() => rive && rive.play()}
+    >
+      <RiveComponent className={`${style.rive}`} />
+      <GlassDiv className={`${style.glassDivTop} ${style.glassDiv}`}>
+        <h2>I'm a student, studying Software Engineering at</h2>
+        <a
+          className={`${style.sust}`}
+          href="https://www.sust.edu/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Shahjalal University of Science and Technology, Sylhet{" "}
+        </a>
+      </GlassDiv>
 
-
-    return (
-        <div className={`${style.body}`}
-            style={{
-                backgroundImage: `url(${pattern === 1 ? svg : pattern === 2 ? svg1 : pattern === 3 ? svg2 : pattern === 4 ? svg3 : svg4})`,
-                // backgroundImage: `url(${svg2})`,
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundColor: "#000000"
-            }}
-            onMouseEnter={() => rive && rive.play()}>
-            <RiveComponent className={`${style.rive}`} />
-            <GlassDiv className={`${style.glassDivTop} ${style.glassDiv}`}>
-                <h2>
-                    I'm a student, studying Software Engineering at
-                </h2>
-                <a className={`${style.sust}`} href="https://www.sust.edu/" target="_blank" rel="noreferrer">Shahjalal University of Science and Technology, Sylhet </a>
-            </GlassDiv>
-
-            <div >
-                {/* 
+      <div>
+        {/* 
                  Email : sourav.ahmed5654@gmail.com
                     Github : https://github.com/Sourav9063
                     Website : https://sourav9063.github.io/my_portfolio/
@@ -54,48 +68,77 @@ export default function Contacts() {
 
                     create GlassDiv for all the links
                 */}
-                <GlassDiv>
-                    <a href="mailto:sourav.ahmed5654@gmail.com" target="_blank" rel="noreferrer">sourav.ahmed5654@gmail.com</a>
-                </GlassDiv>
-                <GlassDiv>
-                    <div className="tooltip imposterA"
-                        onMouseEnter={() => setIsCopied(() => false)}
-                        onClick={(e) => {
-                            navigator.clipboard.writeText("+8801760725654");
-                            setIsCopied(() => true)
-                            e.preventDefault();
-                        }}
-                    >+8801760725654
-                        <span className="glass tooltiptext  text_not_selectable">{isCopied ? "Copied" : "Click To Copy"}</span>
-                    </div>
-                </GlassDiv>
-                <GlassDiv>
-                    <div className="tooltip imposterA"
-                        onMouseEnter={() => setIsCopied(() => false)}
-                        onClick={(e) => {
-                            navigator.clipboard.writeText("+8801760725654");
-                            setIsCopied(() => true)
-                            e.preventDefault();
-                        }}
-                    >+8801775767596
-                        <span className="glass tooltiptext  text_not_selectable">{isCopied ? "Copied" : "Click To Copy"}</span>
-                    </div>
-                </GlassDiv>
-                <GlassDiv>
-                    <a href="https://www.facebook.com/Sourav.Ahmed63/" target="_blank" rel="noreferrer">FaceBook</a>
-                </GlassDiv>
-                <GlassDiv>
-                    <a href="https://www.linkedin.com/in/sourav-ahmed/" target="_blank" rel="noreferrer">LinkedIn</a>
-                </GlassDiv>
+        <GlassDiv>
+          <a
+            href="mailto:sourav.ahmed5654@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            sourav.ahmed5654@gmail.com
+          </a>
+        </GlassDiv>
+        <GlassDiv>
+          <div
+            className="tooltip imposterA"
+            onMouseEnter={() => setIsCopied(() => false)}
+            onClick={(e) => {
+              navigator.clipboard.writeText("+8801760725654");
+              setIsCopied(() => true);
+              e.preventDefault();
+            }}
+          >
+            +8801760725654
+            <span className="glass tooltiptext  text_not_selectable">
+              {isCopied ? "Copied" : "Click To Copy"}
+            </span>
+          </div>
+        </GlassDiv>
+        <GlassDiv>
+          <div
+            className="tooltip imposterA"
+            onMouseEnter={() => setIsCopied(() => false)}
+            onClick={(e) => {
+              navigator.clipboard.writeText("+8801760725654");
+              setIsCopied(() => true);
+              e.preventDefault();
+            }}
+          >
+            +8801775767596
+            <span className="glass tooltiptext  text_not_selectable">
+              {isCopied ? "Copied" : "Click To Copy"}
+            </span>
+          </div>
+        </GlassDiv>
+        <GlassDiv>
+          <a
+            href="https://www.facebook.com/Sourav.Ahmed63/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            FaceBook
+          </a>
+        </GlassDiv>
+        <GlassDiv>
+          <a
+            href="https://www.linkedin.com/in/sourav-ahmed/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </GlassDiv>
+      </div>
 
-
-
-            </div>
-
-        </div>
-    )
+      <GlassDiv>
+        <form className={`${style.emailSection}`}>
+          <input type="email" name="email" />
+          <input type="text" name="subject" />
+          <textarea name="body" id="" cols="30" rows="10"></textarea>
+        </form>
+      </GlassDiv>
+    </div>
+  );
 }
-
 
 /* <div className={`${style.email}`}
     onClick={(e) => {
@@ -106,3 +149,22 @@ export default function Contacts() {
 >
     <h1>Email : sourav.ahmed5654@gmail.com</h1><span> hii</span>
 </div> */
+
+
+{
+    // router.push("/" + MODAL_QUERY_POST);
+//     const res = await fetch("/api/email-sender/from-portfolio", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         email: "email@gmail.com",
+//         username: "username@gmail.com",
+//         emailSubject: "emailSubject",
+//         emailBody: "emailBody",
+//       }),
+//     });
+//     const result = await res.json();
+//     console.log(result);
+//   }
