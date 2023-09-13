b16715ddfc632e5150dba92839c20e0e5924ac57
Sourav <sourav.shellbeehaken@gmail.com>
2023-09-14 00:12:35 +0600

    helper function added

--- src/global/helper/function/ui.js
+++ src/global/helper/function/ui.js
@@ -4,3 +4,9 @@ export const randomRGBA = (alpha = 0.35) => {
   const b = Math.floor(Math.random() * 255);
   return `rgba(${r},${g},${b},${alpha})`;
 };
+
+export const isIn = (start, value, end) => {
+  return start <= value && value <= end;
+};
+
+export const clamp = (num, min, max) => Math.max(min, Math.min(num, max));
