7c72d3a842b69f23c66d8c264ffdff79804375c9
Sourav9063 <sourav.ahmed5654@gmail.com>
2023-12-05 01:55:59 +0600

    removed unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./global/components/navbar/Navbar";
 import "./global/css/global.css";
-import About from "./pages/about/About";
 import Cv from "./pages/cv/Cv";
 import Home from "./pages/home/Home";
-import Projects from "./pages/projects/Projects.jsx";
-import { Suspense } from "react";
-import Links from "./global/components/links/Links";
-import GlobalProvider from "./global/provider/GlobalProvider";
 function App() {
   return (
     // <Routes>
