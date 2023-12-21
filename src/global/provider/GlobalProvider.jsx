f7519e18e95d6b29c42829b423f865580a0ee1f1
Sourav9063 <sourav.ahmed5654@gmail.com>
2023-12-22 03:23:03 +0600

    scroll optimization

--- src/global/provider/GlobalProvider.jsx
+++ src/global/provider/GlobalProvider.jsx
@@ -4,11 +4,13 @@ import React, {
   useContext,
   useEffect,
   useCallback,
+  useRef,
+  useLayoutEffect,
 } from "react";
 import styles from "../../pages/home/Home.module.css";
 export const GlobalContext = createContext();
-let temp = 0;
 export default function GlobalProvider({ children }) {
+  const temp = useRef(0);
   const [scrollPositionOfElement, setScrollPositionOfElement] = useState(0);
   const handleScroll = useCallback((e) => {
     const setScrollPositionOfElementTmp = Math.ceil(
@@ -17,24 +19,29 @@ export default function GlobalProvider({ children }) {
         100 *
         -1
     );
-    if (
-      Math.abs(temp - setScrollPositionOfElementTmp) > 5 ||
-      setScrollPositionOfElementTmp % 100 < 5 ||
-      setScrollPositionOfElementTmp % 100 > 95
-    ) {
-      temp = setScrollPositionOfElementTmp;
-      if (Math.abs(scrollPositionOfElement - setScrollPositionOfElementTmp) > 2)
-        setScrollPositionOfElement(setScrollPositionOfElementTmp);
+    if (Math.abs(temp.current - setScrollPositionOfElementTmp) > 5) {
+      let rounded = setScrollPositionOfElementTmp;
+      if (
+        setScrollPositionOfElementTmp % 100 < 8 ||
+        setScrollPositionOfElementTmp % 100 > 92
+      ) {
+        rounded = Math.round(setScrollPositionOfElementTmp / 100) * 100;
+      }
+      if (rounded !== temp.current) {
+        setScrollPositionOfElement(rounded);
+        console.log(rounded);
+      }
+      temp.current = rounded;
     }
   }, []);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     document
       .querySelector("." + styles.container)
       ?.addEventListener("scroll", handleScroll);
 
     return () => {};
-  }, []);
+  }, [handleScroll]);
   return (
     <GlobalContext.Provider
       value={{ scrollPositionOfElement, setScrollPositionOfElement }}
