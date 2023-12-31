4afbd45a9a76c1d63dfd35b9e5124b4f59f0ea88
Sourav9063 <sourav.ahmed5654@gmail.com>
2023-12-31 19:03:02 +0600

    error fix

--- src/global/provider/ProviderWrapper.jsx
+++ src/global/provider/ProviderWrapper.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ProjectScrollProvider from "./ProjectScrollProvider";
 import GlobalProvider from "./GlobalProvider";
 
 export default function ProviderWrapper({ children }) {
